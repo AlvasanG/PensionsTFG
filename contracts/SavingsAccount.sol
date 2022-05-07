b009fab5caf13262c40f6ce2f3fda9032a2fffd9
Alvaro Sanchez <varosanchezgar5@gmail.com>
2022-05-07 18:44:15 +0200

    Added code comments to SavingsAccount

--- contracts/SavingsAccount.sol
+++ contracts/SavingsAccount.sol
@@ -1,26 +1,36 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.6.0;
 
+/// @title A mock of a savings account
+/// @author Alvaro Sánchez García
 contract SavingsAccount {
     mapping(address => uint256) public balanceOf;
 
+    /// Deposits a given amount on the account
+    /// @param amount The new value to be added to the account
     function deposit(uint256 amount) public payable {
         require(msg.value == amount);
         balanceOf[msg.sender] += amount;
     }
 
+    /// Withdraws a given amount from the account
+    /// @param amount The value to be withdrawn
     function withdraw(uint256 amount) public payable {
         require(amount <= balanceOf[msg.sender]);
         balanceOf[msg.sender] -= amount;
         msg.sender.transfer(amount);
     }
 
+    /// Withdraws the total amount on the account
     function withdrawAll() public payable {
         uint256 fullAmount = balanceOf[msg.sender];
         balanceOf[msg.sender] = 0;
         msg.sender.transfer(fullAmount);
     }
 
+    /// Transfer a given amount between this account and another address
+    /// @param amount The value to transfer
+    /// @param toAccount The account to tranfer the funds to
     function transferFunds(uint256 amount, address toAccount) public payable {
         require(amount <= balanceOf[msg.sender]);
         balanceOf[msg.sender] -= amount;
@@ -28,6 +38,8 @@ contract SavingsAccount {
         require(success, "Transfer failed");
     }
 
+    /// Return the balance of the account
+    /// @return The balance of the account
     function getBalance() public view returns (uint256) {
         return balanceOf[msg.sender];
     }
